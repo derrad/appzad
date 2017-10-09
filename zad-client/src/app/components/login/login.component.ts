d383375b2eb26da7c94dd9a0411ef335dcf5fc36
derrad <radivoje@deretic.net>
2017-10-09 16:05:10 +0200

    sredjivanje izgleda

--- zad-client/src/app/components/login/login.component.ts
+++ zad-client/src/app/components/login/login.component.ts
@@ -3,8 +3,6 @@ import {AuthService} from '../../services/auth/auth.service';
 import {FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {FlashMessagesService} from 'angular2-flash-messages';
 import {Router} from '@angular/router';
-// import {Message} from 'primeng/components/common/api';
-// import {MessageService} from 'primeng/components/common/messageservice';
 
 @Component({
   selector: 'app-login',
@@ -32,29 +30,12 @@ export class LoginComponent implements OnInit {
         this.flashMessage.show('You are now logged in', {
           cssClass: 'alert-success',
           timeout: 5000});
-        
-          // this.msgs.push(
-          //   {
-          //       severity: 'info',
-          //       summary: 'Info Message',
-          //       detail: 'You are now logged in'
-          //   });
-        alert("Logovan sam");
-        this.router.navigate(['dashboard']);
+         this.router.navigate(['dashboard']);
       } else {
-        // this.messageService.add({severity:'error', summary:'Login error', detail:data.msg});
          this.flashMessage.show(data.msg, {
          cssClass: 'alert-danger',
           timeout: 5000});
-
-        // this.msgs.push(
-        //   {
-        //       severity: 'error',
-        //       summary: 'Error Message',
-        //       detail: data.msg
-        //   });
-
-        this.router.navigate(['login']);
+         this.router.navigate(['login']);
       }
     }); 
   }
