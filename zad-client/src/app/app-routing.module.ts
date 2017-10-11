5c3a4e2326f52fc0b29e7d6166277a2dcf5f2289
derrad <radivoje@deretic.net>
2017-10-11 16:01:04 +0200

    Meni i sidebar

---
+++ zad-client/src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+
+
+const routes: Routes = [
+    {
+        path: '',
+        loadChildren: './layout/layout.module#LayoutModule',
+        canActivate: [AuthGuard]
+    },
+    // { path: 'login', loadChildren: './login/login.module#LoginModule' },
+    // { path: 'signup', loadChildren: './signup/signup.module#SignupModule' },
+    {path:'register', component: RegisterComponent},
+    {path:'login', component: LoginComponent},
+    { path: 'not-found', component: NotFoundComponent },
+    { path: '**', redirectTo: 'not-found' }
+    // { path: 'not-found', loadChildren: './not-found/not-found.module#NotFoundModule' },
+    // { path: '**', redirectTo: 'not-found' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- zad-client/src/app/app.module.ts
+++ zad-client/src/app/app.module.ts
@@ -6,14 +6,14 @@ import { HttpModule } from '@angular/http';
 import {RouterModule, Routes} from '@angular/router'; 
 import { ButtonModule,InputTextModule } from 'primeng/primeng';
 import { DataTableModule,SharedModule } from 'primeng/primeng';
-import {FlashMessagesModule} from 'angular2-flash-messages';
+//import {FlashMessagesModule} from 'angular2-flash-messages';
 import {ConfirmDialogModule,ConfirmationService} from 'primeng/primeng';
 import {DialogModule} from 'primeng/primeng';
 import { AppComponent } from './app.component';
 
 
 //Meni and app komponente
-import { HomeComponent } from './components/home/home.component';
+//import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
@@ -49,59 +49,56 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { DrzaveFormComponent } from './components/drzave/drzave-form/drzave-form.component';
 import { ParmetarFormComponent } from './components/parametar/parmetar-form/parmetar-form.component'; 
 import { OpstineFormComponent } from './components/opstine/opstine-form/opstine-form.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const appRoutes: Routes =  [
-  {path:'', component: HomeComponent},
-  {path:'register', component: RegisterComponent},
-  {path:'login', component: LoginComponent},
-  {path:'dashboard', component: DashboardComponent, canActivate:[AuthGuard]},
-  {path:'profile', component: ProfileComponent, canActivate:[AuthGuard]},
-  {path:'drzave', component: DrzaveComponent, canActivate:[AuthGuard]},
-  {path:'drzave/new', component: DrzaveFormComponent, canActivate:[AuthGuard]},
-  {path:'drzave/:id', component: DrzaveFormComponent, canActivate:[AuthGuard]},
-  {path:'parametar', component: ParametarComponent, canActivate:[AuthGuard]},
-  {path:'parametar/new', component: ParmetarFormComponent, canActivate:[AuthGuard]},
-  {path:'parametar/:id', component: ParmetarFormComponent, canActivate:[AuthGuard]},
-  {path:'opstine', component: OpstineComponent, canActivate:[AuthGuard]},
-  {path:'opstine/new', component: OpstineFormComponent, canActivate:[AuthGuard]},
-  {path:'opstine/:id', component: OpstineFormComponent, canActivate:[AuthGuard]},
-  {path:'radnik', component: RadnikComponent, canActivate:[AuthGuard]},
-  {path:'opstine', component: OpstineComponent, canActivate:[AuthGuard]},
-  {path:'mesta', component: MestaComponent, canActivate:[AuthGuard]},
-  {path:'vlasnik', component: VlasnikComponent, canActivate:[AuthGuard]},
-  {path:'fondsati', component: FondSatiComponent, canActivate:[AuthGuard]},
-  {path:'konstanta', component: KonstantaComponent, canActivate:[AuthGuard]},
-  { path: 'not-found', component: NotFoundComponent },
-  { path: '**', redirectTo: 'not-found' }
-  
-  
-  // {path:'drzave', component: SfdrzaveComponent, canActivate:[AuthGuard]},
-  // {path:'adddrzave', component: AdddrzaveComponent, canActivate:[AuthGuard]}
-] 
+// const appRoutes: Routes =  [
+//   {path:'', component: HomeComponent},
+//   {path:'register', component: RegisterComponent},
+//   {path:'login', component: LoginComponent},
+//   {path:'dashboard', component: DashboardComponent, canActivate:[AuthGuard]},
+//   {path:'profile', component: ProfileComponent, canActivate:[AuthGuard]},
+//   {path:'drzave', component: DrzaveComponent, canActivate:[AuthGuard]},
+//   {path:'drzave/new', component: DrzaveFormComponent, canActivate:[AuthGuard]},
+//   {path:'drzave/:id', component: DrzaveFormComponent, canActivate:[AuthGuard]},
+//   {path:'parametar', component: ParametarComponent, canActivate:[AuthGuard]},
+//   {path:'parametar/new', component: ParmetarFormComponent, canActivate:[AuthGuard]},
+//   {path:'parametar/:id', component: ParmetarFormComponent, canActivate:[AuthGuard]},
+//   {path:'opstine', component: OpstineComponent, canActivate:[AuthGuard]},
+//   {path:'opstine/new', component: OpstineFormComponent, canActivate:[AuthGuard]},
+//   {path:'opstine/:id', component: OpstineFormComponent, canActivate:[AuthGuard]},
+//   {path:'radnik', component: RadnikComponent, canActivate:[AuthGuard]},
+//   {path:'opstine', component: OpstineComponent, canActivate:[AuthGuard]},
+//   {path:'mesta', component: MestaComponent, canActivate:[AuthGuard]},
+//   {path:'vlasnik', component: VlasnikComponent, canActivate:[AuthGuard]},
+//   {path:'fondsati', component: FondSatiComponent, canActivate:[AuthGuard]},
+//   {path:'konstanta', component: KonstantaComponent, canActivate:[AuthGuard]},
+//   { path: 'not-found', component: NotFoundComponent },
+//   { path: '**', redirectTo: 'not-found' }
+// ] 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+  //  HomeComponent,
     LoginComponent,
     DashboardComponent,
-    ProfileComponent,
+   ProfileComponent,
     RegisterComponent,
     NavbarComponent,
     NavbarLeftComponent,
     ModalComponent,
     DrzaveComponent,
-    ParametarComponent,
+   ParametarComponent,
     RadnikComponent,
     OpstineComponent,
     MestaComponent,
     VlasnikComponent,
     FondSatiComponent,
     KonstantaComponent,
-    NotFoundComponent,
-    DrzaveFormComponent,
-    ParmetarFormComponent,
-    OpstineFormComponent
+     DrzaveFormComponent,
+     ParmetarFormComponent,
+     OpstineFormComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
@@ -109,14 +106,15 @@ const appRoutes: Routes =  [
     FormsModule,
     ReactiveFormsModule ,
     HttpModule,
-    RouterModule.forRoot(appRoutes), 
-    FlashMessagesModule,
+    //RouterModule.forRoot(appRoutes), 
+    // FlashMessagesModule,
     ButtonModule,
     DataTableModule,
     SharedModule,
     InputTextModule,
     ConfirmDialogModule,
-    DialogModule
+    DialogModule,
+    AppRoutingModule
 
   ],
   providers: [ValidateService, AuthService,AuthGuard,
