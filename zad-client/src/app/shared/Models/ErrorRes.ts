32594f1c63fb49ad21d503b5cd01ec650c81c5de
derrad <radivoje@deretic.net>
2017-10-30 10:08:50 +0100

    Obrisao stari model restcustom

--- zad-client/src/app/shared/Models/ErrorRes.ts
+++ zad-client/src/app/shared/Models/ErrorRes.ts
@@ -16,6 +16,19 @@ export class ErrorRespone  extends  Serializable{
     headers:any;
     type:Number;
     url: string;
-  
 
 }
+
+// export interface IRest {
+//     success:boolean;
+//     message: string;
+//     data: Array<Object>;
+//     ver?:string;
+// }
+
+// export class Rest implements IRest{
+//     success:boolean;
+//     message: string;
+//     data: Array<Object>;
+//     ver?:string;
+// }
